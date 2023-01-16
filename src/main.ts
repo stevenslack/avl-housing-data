0a7fbe212f3625d03989ede49b4a8f54ef63ef01
Steven Slack <stevenslack@gmail.com>
2023-01-16 08:24:36 -0500

    add further comments for clarity

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,10 @@ const {
 const width = 1000;
 const height = 600;
 
-// Select the existing SVG element.
+/**
+ * Target the existing SVG element to build the
+ * line chart and set the dynamic width and height.
+ */
 const svg = d3.select('.pe-graph__svg')
   .attr('width', width)
   .attr('height', height)
@@ -25,18 +28,50 @@ const svg = d3.select('.pe-graph__svg')
 
 /**
  * Set up the scale and path (for the line) of the SVG.
+ *
+ * Time scales implement ticks based on calendar intervals. Since
+ * our X axis is a time scale this is the function to generate the
+ * X scale.
+ *
+ * The extent of the range is the determined with the dataSeries dateRange
+ * and the width determines the scale of the range.
+ *
+ * @param range — Array of range values.
  */
 const xScale: d3.ScaleTime<number, number, never> = d3.scaleTime()
   .domain(
     d3.extent(dataSeries, (d: PEdataPoint) => d.dateRange?.[1]) as Date[],
   )
+  // Sets the scale’s range to the specified array of values.
   .range([0, width]);
 
+/**
+ * X Axis generator.
+ *
+ * Constructs a new bottom-oriented axis generator for the given scale.
+ */
+const xAxis: AxisGenerator = d3.axisBottom(xScale);
+
+/**
+ * Set the Y linear scale defined over a numeric domain (PEratio).
+ *
+ * @param range — Array of range values.
+ */
 const yScale: d3.ScaleLinear<number, number, never> = d3.scaleLinear()
   // The domain gives us the y-axis range starting at 4.
   .domain([4, d3.max(dataSeries, (d: PEdataPoint) => d.PEratio) as number + 1])
   .range([height, 10]);
 
+/**
+ * Y axis generator.
+ *
+ * Constructs a new left-oriented axis generator for the given scale.
+ */
+const yAxis: AxisGenerator = d3.axisLeft(yScale);
+
+/**
+ * Line generator for the SVG path values in the line chart.
+ */
 const lineGenerator = d3.line()
   .x((d: PEdataPoint | [number, number]) => {
     if ('dateRange' in d) {
@@ -54,44 +89,50 @@ const lineGenerator = d3.line()
   })
   .curve(d3.curveMonotoneX);
 
+/**
+ * Create the SVG path element and generate the line.
+ */
 svg.append('path')
   .datum(dataSeries)
   .attr('d', lineGenerator as LineGenerator)
   .attr('stroke', 'steelblue')
   .attr('fill', 'none');
 
-// Average line.
+/**
+ * Create the horizontal average line
+ * which demonstrates the average P/E over time.
+ */
 d3.select('.pe-graph__avg-line')
   .attr('x2', `${width}`)
   .attr('y1', yScale(PEavg))
   .attr('y2', yScale(PEavg));
 
 /**
- * X Axis set up.
- *
- * Constructs a new bottom-oriented axis generator for the given scale.
- * @see https://github.com/d3/d3-axis#axisBottom
+ * Create the X Axis for the bottom of the graph
+ * which displays the ticks as year values.
  */
-const xAxis: AxisGenerator = d3.axisBottom(xScale);
-
 svg.select('.pe-graph__x-axis')
   // The transform puts the x axis at the bottom of the graph.
   .attr('transform', `translate(0, ${height})`)
   .call(xAxis);
 
-// Add the X Axis label.
+/**
+ * Add the label and set the position for the X Axis.
+ */
 svg.select('.pe-graph__x-axis-label')
   .attr('x', `${width / 2}`)
   .attr('y', 50);
 
 /**
- * Y axis set up.
+ * Create the Y Axis for the left side of the graph
+ * which displays the ticks as price to earnings values.
  */
-const yAxis: AxisGenerator = d3.axisLeft(yScale);
-
 svg.select('.pe-graph__y-axis')
   .call(yAxis);
 
+/**
+ * Add the label and set the position for the Y Axis.
+ */
 svg.select('.pe-graph__y-axis-label')
   .attr('x', `-${height / 2}`)
   .attr('y', -50);
